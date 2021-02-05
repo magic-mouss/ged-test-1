fdc4c5d17d0a14d53e3057899bb2723c7c012e08
my name <anli.mouslim9@gmail.com>
2021-02-05 10:15:02 +0100

    envoie du 502-session

--- src/Controller/AuthentificationController.php
+++ src/Controller/AuthentificationController.php
@@ -12,41 +12,85 @@ use App\Entity\Utilisateur;
 class AuthentificationController extends AbstractController
 {
 /**
-* @Route("/authentification", name="authentification")
-*/
+ * @Route("/authentification", name="authentification")
+ */
 public function index(): Response
 {
-return $this->render('authentification/index.html.twig', [
-'controller_name' => 'AuthentificationController',
-]);
+	return $this->render('authentification/index.html.twig', [
+	'controller_name' => 'AuthentificationController',
+	]);
 }
 
 /**
-* @Route("/insertUser", name="insertUser")
-*/
+ * @Route("/insertUser", name="insertUser")
+ */
 public function insertUser(): Response
 {
-return $this->render('authentification/insertUser.html.twig', [
-'controller_name' => "Insertion d'un nouvel Utilisateur",
-]);
+	return $this->render('authentification/insertUser.html.twig', [
+	'controller_name' => "Insertion d'un nouvel Utilisateur",
+	]);
 }
 
 /**
-* @Route("/insertUserBdd", name="insertUserBDD")
-*/
+ * @Route("/insertUserBdd", name="insertUserBDD")
+ */
 public function insertUserBdd(Request $request, EntityManagerInterface $manager): Response
 {
-$User = new Utilisateur();
-$User->setNom($request->request->get('nom'));
-$User->setPrenom($request->request->get('prenom'));
-$User->setCode($request->request->get('code'));
-$User->setSalt($request->request->get('salt'));
+	$User = new Utilisateur();
+	$User->setNom($request->request->get('nom'));
+	$User->setPrenom($request->request->get('prenom'));
+	$User->setCode($request->request->get('code'));
+	$User->setSalt($request->request->get('salt'));
 
-$manager->persist($User);
-$manager->flush();
+	$manager->persist($User);
+	$manager->flush();
 
-return $this->render('authentification/insertUser.html.twig', [
-'controller_name' => "Ajout en base de données.",
-]);
+	return $this->render('authentification/insertUser.html.twig', [
+	'controller_name' => "Ajout en base de données.",
+	]);
+}
+/**
+ * @Route("/connexion", name="connexion")
+ */
+public function connexion(Request $request, EntityManagerInterface $manager): Response
+{
+	//Récupération des identifiants de connexion
+	$identifiant = $request->request->get('login');
+	$password = $request->request->get('password');
+	//Test de l'existence d'un tel couple
+	$aUser = $manager->getRepository(Utilisateur::class)->findBy(["nom"=>$identifiant, "code"=>$password]);
+	if (sizeof($aUser)>0) {
+		return $this->redirectToRoute('dashboard');
+	}else{
+		return $this->redirectToRoute('authentification');
+	}if (sizeof($aUser)>0){
+		$utilisateur = new Utilisateur;
+		$utilisateur = $aUser[0];
+		//démarrage des variables de session
+		$sess = $request->getSession();
+		//Information de session
+		$sess->set("idUtilisateur", $utilisateur->getId());
+		$sess->set("nomUtilisateur", $utilisateur->getNom());
+		$sess->set("prenomUtilisateur", $utilisateur->getPrenom());
+
+		return $this->redirectToRoute('dashboard');
+	}else{
+		return $this->redirectToRoute('authentification');
+	}
+	dd($identifiant, $password, $reponse);
+	return new response(1);
+	// return $this->render('authentification/insertUser.html.twig', [
+			// 'controller_name' => "Ajout en base de données.",
+		// ]);
+}
+/**
+ * @Route("/dashboard", name="dashboard")
+ */
+public function dashboard(Request $request, EntityManagerInterface $manager): Response
+{
+	$sess = $request->getSession();
+	return $this->render('authentification/dashboard.html.twig',[
+	'controller_name' => "Espace Client",
+	]);
 }
 }
